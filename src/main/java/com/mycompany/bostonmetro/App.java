273d9b959c7fdba056c2ec4f9abeb5ea8dbd2ba1
Colin But <colinbut@users.noreply.github.com>
2015-04-25 11:39:59 +0100

    initial project files

---
+++ src/main/java/com/mycompany/bostonmetro/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.bostonmetro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
